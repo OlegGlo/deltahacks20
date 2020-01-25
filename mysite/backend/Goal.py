46e8ea7906493ae389f808d27d39aa9806ab8245
Sawyer <sawyercoletang@gmai.com>
2020-01-25 17:09:29 -0500

    updated min and max goal classes by adding more attributes... Note: toString funcitons are not updated

--- mysite/backend/Goal.py
+++ mysite/backend/Goal.py
@@ -1,4 +1,4 @@
-# Parent Class
+# Parent Class - not instantiated
 class Goal(object):
 
     def __init__(self, title, desc):
@@ -16,28 +16,42 @@ class YesNoGoal(Goal):
         super(YesNoGoal, self).__init__(title, desc)
 
     def toString(self):
-        return "=== YesNoGoal ===\n" + str(super(YesNoGoal, self).toString()) + "\n"
+        return "=== YesNoGoal ===\n" + super(YesNoGoal, self).toString() + "\n"
+
+# QuantitativeGoal Class - not instantiated
+class QuantitativeGoal(Goal):
+
+    def __init__(self, title, desc, metric, goalQuantity):
+        super(QuantitativeGoal, self).__init__(title, desc)
+        self.metric = metric
+        self.goalQuantity = goalQuantity
+
+    def toString(self):
+        return super(QuantitativeGoal, self).toString()
 
 # MinGoal Class
-class MinGoal(Goal):
-    def __init__(self, title, desc):
-        super(MinGoal, self).__init__(title, desc)
+class MinGoal(QuantitativeGoal):
+    def __init__(self, title, desc, metric, goalQuantity):
+        super(MinGoal, self).__init__(title, desc, metric, goalQuantity)
 
     def toString(self):
-        return "=== MinGoal ===\n" + str(super(MinGoal, self).toString()) + "\n"
+        return "=== MinGoal ===\n" + super(MinGoal, self).toString() + "\n"
 
 # MaxGoal Class
-class MaxGoal(Goal):
-    def __init__(self, title, desc):
-        super(MaxGoal, self).__init__(title, desc)
+class MaxGoal(QuantitativeGoal):
+    def __init__(self, title, desc, metric, goalQuantity):
+        super(MaxGoal, self).__init__(title, desc, metric, goalQuantity)
 
     def toString(self):
-        return "=== MaxGoal ===\n" + str(super(MaxGoal, self).toString()) + "\n"
+        return "=== MaxGoal ===\n" + super(MaxGoal, self).toString() + "\n"
 
 
 
 # g = Goal("bruh", "be bruhed")
 # print(g.toString())
 
-yn = YesNoGoal("this is a goal", "achieve this goal!")
+# yn = YesNoGoal("this is a goal", "achieve this goal!")
+# print(yn.toString())
+
+yn = MaxGoal("this is a goal", "achieve this goal!", "pushups", 10)
 print(yn.toString())
